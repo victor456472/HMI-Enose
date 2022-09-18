1174b60cb3abeb3ac8beec1279e2b5f0d85baf31
victor456472 <victor456472@gmail.com>
2022-09-18 10:10:23 -0500

    creando proyecto

---
+++ main.py
@@ -0,0 +1,135 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
+from PyQt5.QtCore import QIODevice, QPoint
+from PyQt5 import QtCore, QtWidgets
+import pyqtgraph as pg
+import numpy as np
+from Design import Ui_MainWindow
+
+class Application(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initGui()
+    
+    def initGui(self):
+
+        #se crea un objeto de la clase UiMainWindow que contiene los elementos de la interfaz grafica
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        #estas declaraciones iniciales permiten programar los botones de la ventana (maximizar, minimizar, restaurar y cerrar)
+        #ademas se obtiene la posicion del cursor
+        self.ui.boton_normalizar.hide()
+        self.click_position=QPoint()
+        self.ui.boton_minimizar.clicked.connect(lambda: self.showMinimized())
+        self.ui.boton_normalizar.clicked.connect(self.control_normalizar)
+        self.ui.boton_maximizar.clicked.connect(self.control_maximizar)
+        self.ui.boton_salir.clicked.connect(lambda: self.close())
+
+        #eliminar la barra de titulo y la opacidad
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setWindowOpacity(1)
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+        #sizeGrip
+        self.gripSize=10
+        self.grip= QtWidgets.QSizeGrip(self)
+        self.grip.resize(self.gripSize, self.gripSize)
+
+        #mover ventana
+        self.ui.frame_superior.mouseMoveEvent = self.mover_ventana
+
+        #conexion serial
+        self.serial=QSerialPort()
+        self.ui.boton_actualizar.clicked.connect(self.read_ports)
+        self.ui.boton_conectar.clicked.connect(self.serial_connect)
+        self.ui.boton_desconectar.clicked.connect(lambda: self.serial.close())
+
+        #lectura de datos
+        self.serial.readyRead.connect(self.read_data)
+        self.x = list(np.linspace(0,100,100))
+        self.y = list(np.linspace(0,0,100))
+
+        #grafica
+        pg.setConfigOption('background', '#2c2c2c')
+        pg.setConfigOption('foreground', '#ffffff')
+        self.plt = pg.PlotWidget(title='grafica')
+        self.ui.grafica.addWidget(self.plt)
+
+        self.read_ports()
+    
+    def read_ports(self):
+        self.baudrates = ['1200', '2400', '4800', '9600', '19200', '38400', '115200']
+        portList = []
+        ports = QSerialPortInfo().availablePorts()
+        for port in ports:
+            portList.append(port.portName())
+        self.ui.comboBox_puerto.clear()
+        self.ui.comboBox_baudrate.clear()
+        self.ui.comboBox_puerto.addItems(portList)
+        self.ui.comboBox_baudrate.addItems(self.baudrates)
+        self.ui.comboBox_baudrate.setCurrentText('9600')
+    
+    def serial_connect(self):
+        self.serial.waitForReadyRead(100)
+        self.port = self.ui.comboBox_puerto.currentText()
+        self.baud = self.ui.comboBox_baudrate.currentText()
+        self.serial.setBaudRate(int(self.baud))
+        self.serial.setPortName(self.port)
+        self.serial.open(QIODevice.ReadWrite)
+
+    def read_data(self):
+        if not self.serial.canReadLine(): return
+        rx = self.serial.readLine()
+        x=str(rx, 'utf-8').strip()
+        x=float(x)
+        print(x)
+        self.y = self.y[1:]
+        self.y.append(x)
+        self.plt.clear()
+        self.plt.plot(self.x,self.y,pen=pg.mkPen('#da0037', width=2))
+    
+    def control_normalizar(self):
+        self.showNormal()
+        self.ui.boton_normalizar.hide()
+        self.ui.boton_maximizar.show()
+    
+    def control_maximizar(self):
+        self.showMaximized()
+        self.ui.boton_maximizar.hide()
+        self.ui.boton_normalizar.show()
+
+    #tamaño de la ventana
+    def resizeEvent(self, event):
+        rect = self.rect()
+        self.grip.move(rect.right() - self.gripSize, rect.bottom() - self.gripSize) 
+    
+    #mover la ventana
+    def mousePressEvent(self, event):
+        self.click_position = event.globalPos()
+    
+    def mover_ventana(self, event):
+        if self.isMaximized()==False:
+            if event.buttons() == QtCore.Qt.LeftButton:
+                self.move(self.pos() + event.globalPos() - self.click_position)
+                self.click_position = event.globalPos()
+                event.accept()
+        if event.globalPos().y() <= 5 or event.globalPos().x() <= 5:
+            self.showMaximized()
+            self.ui.boton_maximizar.hide()
+            self.ui.boton_normalizar.show()
+        else:
+            self.showNormal()
+            self.ui.boton_normalizar.hide()
+            self.ui.boton_maximizar.show()
+    
+def main():
+    app = QApplication(sys.argv)
+    ventana = Application()
+    ventana.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
